bef2e815a7373fad189fe5882f0a7f008d306ea3
Mark <mark@iMac2.local>
2018-12-04 13:40:35 -0500

    created the green background

--- unit_ten_warmups.py
+++ unit_ten_warmups.py
@@ -1,10 +1,13 @@
 import pygame, sys
 from pygame.locals import*
+import SSFS_logo
 
 pygame.init()
-mainSurface = pygame.display.set_mode((900, 500), 0, 32)
+mainSurface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("SSFS")
 
+myTarget = SSFS_logo.Logo(mainSurface)
+myTarget.draw_logo()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
